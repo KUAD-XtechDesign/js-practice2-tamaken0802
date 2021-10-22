664dc121f5a57e59b1fa7961b84915e23cde75d8
tamaken0802 <85724739+tamaken0802@users.noreply.github.com>
2021-10-22 13:42:18 +0900

    Update script02.js

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -1,4 +1,38 @@
-$(function(){
+$(function(){ 
+ 
+    let hako = 0;
+    
+    function checkBox(){
+      if(hako > 5){
+        $("#content").text(hako + "個はいっています。箱はいっぱいです。空にします。");
+        $("#content").removeClass().addClass('full')
+        hako = 0;
+      }else if(hako <= 0){
+        hako = 0;
+        $("#content").text(hako + "個。入ってません。");
+        $("#content").removeClass().addClass('empty')
+      }else{
+        $("#content").text(hako + "個はいっています。まだ入ります。");
+        $("#content").removeClass()
+      }
+    }
+    
+    $("#button01").on("click",function(){
+      checkBox();
+    })
+    
+    $("#button02").on("click",function(){
+      hako++;
+      checkBox();
+    })
+    
+    $("#button03").on("click",function(){
+      hako--;
+      checkBox();
+    })
+    
+    
+   
 
 
 
