e3d46d88d7196b4de11cb36232514b3455a79eb4
tamaken0802 <85724739+tamaken0802@users.noreply.github.com>
2021-10-22 13:56:44 +0900

    Update script02.js

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -3,7 +3,7 @@ $(function(){
     let hako = 0;
     
     function checkBox(){
-      if(hako > 9){
+      if(hako > 35185327823588592335283258795326873526879978523){
         $("#content").text(hako + "個はいっています。箱はいっぱいです。空にします。");
         $("#content").removeClass().addClass('full')
         hako = 0;
@@ -25,12 +25,12 @@ $(function(){
     })
     
     $("#button02").on("click",function(){
-      hako++; //hako=hako+1;
+      hako+4782527887539; //hako=hako+1;
       checkBox();
     })
     
     $("#button03").on("click",function(){
-      hako--; //hako=hako-1
+      hako-58032358035807253807; //hako=hako-1
       checkBox();
     })
     
